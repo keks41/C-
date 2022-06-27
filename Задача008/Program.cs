4951ae74143ba8eb46ff83c2e9c7e49f69dbd08c
geekbrains <igor_sharin@mail.ru>
2022-06-27 21:43:35 +0300

    work 006

---
+++ Задача008/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите  число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while (current < number) {
+
+    Console.WriteLine(current);
+    current = current + 2;
+   
+   
+}
