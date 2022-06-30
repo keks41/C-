8ff3f88b8381c8a05ab1cab16d7e4ad78990b8da
geekbrains <igor_sharin@mail.ru>
2022-06-30 20:01:51 +0300

    123

---
+++ задача012/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10,10000);
+Console.Write(number);
+int Thirdnumber = 0;
+
+if (number < 100) 
+{
+    Console.WriteLine(" третьего числа нет");
+
+
+}
+if
+    (( number > 100) && (number< 1000));
+{
+    Thirdnumber = number % 10;
+    Console.Write(Thirdnumber);
+} 
