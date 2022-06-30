52dac6535501e34af9aae8e478aece7fceab9457
geekbrains <igor_sharin@mail.ru>
2022-06-30 21:33:36 +0300

    21

--- задача 017/Program.cs
+++ задача 017/Program.cs
@@ -1,23 +1,44 @@
-﻿void quarter(int x, int y)
+﻿/*void quarter(int x, int y)
 {
-    if (x > 0 , y > 0)
+    if (x > 0, y > 0 );
     {
         Console.WriteLine("1 четверть");
 
     }
-    else if (x<0 , y<0 )
+    else if (x<0, y<0 );
     {
         Console.WriteLine("2 четверть");
 
     }
-    else if (x < 0 , y < 0)
+    else if (x < 0, y < 0);
     {
-        Console.WriteLine("3 четверть");
+        Consol.WriteLine("3 четверть");
 
     }   
-    else if (x > 0, y < 0 )
+    else if (x > 0, y < 0 );
     {
         Console.WriteLine("4 четверть");
 
     }
 }    
+*/
+/*int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine(" x > 0 , y > 0");
+}
+else if (num == 2)
+{
+    Console.WriteLine(" x < 0 , y > 0");
+}
+else if (num == 3)
+{
+    Console.WriteLine(" x < 0 , y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine(" x > 0 , y > 0");
+}
+*/
+
+
---
+++ задача 021/Program.cs
@@ -0,0 +1,13 @@
+﻿double number(double x1, double y1, double x2, double y2 )
+{
+    double result = 0;
+    if (x1==x2 && y1==y2)
+    {
+        return result;
+    }
+    
+    result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)); 
+    return result;
+}
+double A = number(3,6,2,1);
+Console.WriteLine(A);
