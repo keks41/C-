af6a55dd59d12810ed3402921a3b185f36ece388
geekbrains <igor_sharin@mail.ru>
2022-07-01 07:24:28 +0300

    21

---
+++ задача022/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] A = new int[num];
+for(int i=1, i=<num, i++);
+{
+    A[i-1] = i * i;
+    Console.WriteLine(A[i-1])
+}
+*/
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] A = new int[num];
+int i = 1;
+while (i <= num);
+{
+    A[i-1] = i*1;
+    Console.WriteLine(A[i-1]);
+    i++;
+}
